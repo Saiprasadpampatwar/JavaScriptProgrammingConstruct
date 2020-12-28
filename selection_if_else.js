6a6d7d70fcf0b13a3a4a61b5942b857946d9f134
SaiprasadPampatwar <pampatwarsaiprasad321@gmail.com>
2020-12-28 15:04:24 +0530

    [Saiprasad]Added: Ability to check date between given range

--- selection_if_else.js
+++ selection_if_else.js
@@ -12,3 +12,15 @@ for(let i=0;i<4;i++){
 }
 console.log("min number : "+min)
 console.log("max number : "+max)
+
+//2
+let day = process.argv[2];
+let month = process.argv[3];
+
+if((day >= 20 && day <= 31 && month == 'March') || (day >= 1 && day <= 20 && month == 'June') ||
+                         (day >= 1 && day <= 30 && month == 'April') || (day >= 1 && day <= 30 && month == 'May')) {
+        console.log(true);
+}
+else {
+    console.log(false);
+}
