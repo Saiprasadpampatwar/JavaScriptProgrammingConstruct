545c3eb20af28888513b42d5bc47a8f1edf74107
SaiprasadPampatwar <pampatwarsaiprasad321@gmail.com>
2020-12-28 14:01:13 +0530

    [Saiprasad]Added: random fuction to generate a random number on dice

--- sequence_print_random.js
+++ sequence_print_random.js
@@ -1,2 +1,5 @@
 let digit = Math.floor(Math.random()*10);
-console.log(digit);
+console.log("random number between 1 to 10 : "+ digit);
+
+let digit1 = Math.floor(Math.random()*6) +1;
+console.log("number on dice : " + digit1);
