ecaf1f35a51432f50c76b9963415ca2012a978cd
SaiprasadPampatwar <pampatwarsaiprasad321@gmail.com>
2020-12-28 14:04:56 +0530

    [Saiprasad]Added: random fuction to generate a random number on dice and printed sum of two dice number

--- sequence_print_random.js
+++ sequence_print_random.js
@@ -1,5 +1,13 @@
+//1
 let digit = Math.floor(Math.random()*10);
 console.log("random number between 1 to 10 : "+ digit);
 
+//2
 let digit1 = Math.floor(Math.random()*6) +1;
 console.log("number on dice : " + digit1);
+
+//3
+let digit2 = Math.floor(Math.random()*6) +1;
+let digit3 = Math.floor(Math.random()*6) +1;
+let digit4 = digit2 + digit3;
+console.log("sum of numbers on dice : " + digit4);
