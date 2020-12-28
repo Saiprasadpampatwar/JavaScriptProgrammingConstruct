dfc8eb38043e385af7c495490ac1db6f6eee07ba
SaiprasadPampatwar <pampatwarsaiprasad321@gmail.com>
2020-12-28 14:10:57 +0530

    [Saiprasad]Calculated: sum and average of five two digit random values

--- sequence_print_random.js
+++ sequence_print_random.js
@@ -11,3 +11,11 @@ let digit2 = Math.floor(Math.random()*6) +1;
 let digit3 = Math.floor(Math.random()*6) +1;
 let digit4 = digit2 + digit3;
 console.log("sum of numbers on dice : " + digit4);
+
+//4
+let sum = 0;
+for(let i=0;i<5;i++){
+    sum+=Math.floor(Math.random()*100);
+}
+let average = sum/5;
+console.log("sum : "+sum+", Average : "+average)
